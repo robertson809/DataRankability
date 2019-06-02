4271d4e73dde90b497c80b652eb83ac9133f47af
Cameron <thcameron@w16425.davidson.edu>
2019-06-02 15:40:55 -0400

    update

---
+++ MATLAB/writeSouthernMBB.m
@@ -0,0 +1,11 @@
+function writeSouthernMBB()
+A = zeros(14,2);
+for i=2002:2015
+    gamefilename = strcat('../data_files/ConferencesMBB/Southern/', int2str(i),...
+        'games.txt');
+    teamfilename = strcat('../data_files/ConferencesMBB/Southern/', int2str(i),...
+        'teams.txt');
+    A(i-2001,1)=MasseySeasonPercentageCorrect(gamefilename,teamfilename,0);
+    A(i-2001,2)=ColleySeasonPercentageCorrect(gamefilename,teamfilename,0);
+end
+csvwrite('SouthernMBBPredict.csv',A)
