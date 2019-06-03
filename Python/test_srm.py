99e94530c9d7f97e174f0f9ad47a8c7ec3c1864c
Cameron <thcameron@w15166.davidson.edu>
2019-06-03 09:27:23 -0400

    update to python and mathematica

--- Python/test_srm.py
+++ Python/test_srm.py
@@ -2,7 +2,7 @@
 #
 # Author: Thomas R. Cameron
 # Date: 4/9/2019
-from srm_module import rankM, rankH, algCon, rankA, ranking
+from srm_module import specR, connR
 import numpy as np
 
 #####################################################
@@ -31,10 +31,10 @@ import numpy as np
 #####################################################
 #                 Big East D1 NCAA Football         #
 #####################################################
-years = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012]
-for k in range(1,2):
+years = [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012]
+for k in range(len(years)):
     print('Year'+str(years[k])+': ')
-    f = open('../data_files/Big_East_matrices/binaryMatrix'+str(years[k])+'.txt')
+    f = open('../data_files/Big_East_Football/matrices/binaryMatrix'+str(years[k])+'.txt')
     lineList = f.readlines()
     n = len(lineList)
     a = np.zeros((n,n))
@@ -43,8 +43,7 @@ for k in range(1,2):
         row = row.split("   ")
         for j in range(1,n+1):
             a[i][j-1] = row[j]
-    print('rankH = '+str('%.3f' % rankH(a))+' and rankA = '+str('%.3f' % rankA(a))+' rankA+rankH = '+str('%.3f' % (rankA(a)+rankH(a))))
-    print('ranking = '+str(ranking(a))+'\n')
+    print('specR = '+str('%.3f' % specR(a))+' and connR = '+str('%.3f' % connR(a)))
 
 #print('\n')
 #####################################################
