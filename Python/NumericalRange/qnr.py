d8f981a787c8932973eb340436e0a64ccd046e89
Michael Robertson <mirobertson@davidson.edu>
2019-11-26 21:40:25 -0500

    final changes on this branch

--- Python/NumericalRange/qnr.py
+++ Python/NumericalRange/qnr.py
@@ -144,7 +144,6 @@ def is_polygon(f, l):
     evs = np.linalg.eigvals(l)
     twod_evs = np.column_stack((evs.real, evs.imag))
     ordered_vertices = twod_evs[euler_sort(evs)]
-    on_line = False
     count = 0
     # print(f)
     for num in f:
@@ -162,59 +161,59 @@ def is_polygon(f, l):
                                 [num.imag - ordered_vertices[i][1],
                                  ordered_vertices[m][1] - ordered_vertices[i][1]]])
                 if matrix_rank(a, tol=1e-14) == 1:
-                    print('point {} on line {}'.format(count, i))
+                    # print('point {} on line {}'.format(count, i))
                     on_line = True
                     break
-                else:
-                    print('not on line ', i)
+                # else:
+                #     print('not on line ', i)
         if not on_line:
             return False
     print('is a polygon')
     return True
 
 
-def allQNR(n, r_graph_num=-1, disp=False):
-    """
-    Finds and prints ALL the restricted numerical ranges of graphs on n vertices. Does not consider uniqueness
-    around isomorphism
-    :param n: the number of vertices
-    :param r_graph_num: potential to specify a graph or list of graphs rather than all graphs, default -1
-                        prints all graphs
-    :param disp: the option to display pictures of the graphs generated
-    :return:
-    """
-    adj = []
-    # produces a list of flattened matrices with 1,0 entries
-    for i in itertools.product([0, 1], repeat=n * n):
-        a = np.reshape(np.array(i), (n, n))  # don't give a shit if this is unique or not
-        if np.trace(a) == 0:
-            adj.append(a)
+# def allQNR(n, r_graph_num=-1, disp=False):
+#     """
+#     Finds and prints ALL the restricted numerical ranges of graphs on n vertices. Does not consider uniqueness
+#     around isomorphism
+#     :param n: the number of vertices
+#     :param r_graph_num: potential to specify a graph or list of graphs rather than all graphs, default -1
+#                         prints all graphs
+#     :param disp: the option to display pictures of the graphs generated
+#     :return:
+#     """
+#     adj = []
+#     # produces a list of flattened matrices with 1,0 entries
+#     for i in itertools.product([0, 1], repeat=n * n):
+#         a = np.reshape(np.array(i), (n, n))  # don't give a shit if this is unique or not
+#         if np.trace(a) == 0:
+#             adj.append(a)
+#
+#     if r_graph_num != -1:  # turn adj into a singleton or shortened list
+#         if type(r_graph_num) == int:
+#             g = adj[r_graph_num]
+#             adj = [g]
+#         if type(r_graph_num) == list:
+#             adj = [adj[num] for num in r_graph_num]
+#
+#     graph_num = 0
+#     for a in adj:
+#         x = np.array([np.sum(a[i, :]) for i in range(n)])
+#         l = np.diag(x) - a
+#         if disp:
+#             if r_graph_num == -1:
+#                 plt.title('Graph {}'.format(graph_num))
+#             elif type(r_graph_num) == int:
+#                 plt.title('Graph {}'.format(r_graph_num))
+#             elif type(r_graph_num) == list:
+#                 plt.title('Graph {}'.format(r_graph_num[graph_num]))
+#             g = nx.DiGraph(a)
+#             nx.draw(g, with_labels=True, ax=plt.subplot(121))
+#         graph_num += 1
+#         qnr(l, graph_num, disp)
 
-    if r_graph_num != -1:  # turn adj into a singleton or shortened list
-        if type(r_graph_num) == int:
-            g = adj[r_graph_num]
-            adj = [g]
-        if type(r_graph_num) == list:
-            adj = [adj[num] for num in r_graph_num]
 
-    graph_num = 0
-    for a in adj:
-        x = np.array([np.sum(a[i, :]) for i in range(n)])
-        l = np.diag(x) - a
-        if disp:
-            if r_graph_num == -1:
-                plt.title('Graph {}'.format(graph_num))
-            elif type(r_graph_num) == int:
-                plt.title('Graph {}'.format(r_graph_num))
-            elif type(r_graph_num) == list:
-                plt.title('Graph {}'.format(r_graph_num[graph_num]))
-            g = nx.DiGraph(a)
-            nx.draw(g, with_labels=True, ax=plt.subplot(121))
-        graph_num += 1
-        qnr(l, graph_num, disp)
-
-
-def isoQNR(n, r_graph_num=-1, disp=True):
+def all_qnr(n, r_graph_num=-1, disp=True, check_iso=True):
     """
     Finds and prints ALL the restricted numerical ranges of graphs on n vertices. Does not consider uniqueness
     around isomorphism
@@ -230,8 +229,11 @@ def isoQNR(n, r_graph_num=-1, disp=True):
         a = np.reshape(np.array(i), (n, n))
         if (np.trace(a) == 0):
             perm = unique_perm(a, n)
+            if not check_iso:
+                adj.append(a)
+                continue
             add = not any(np.array_equal(p, x) for p in perm for x in adj)
-            if (add):
+            if add:
                 adj.append(a)
     print('There are {} isomorphically unique graphs '
           'with {} vertices'.format(len(adj), n))
@@ -263,8 +265,6 @@ def isoQNR(n, r_graph_num=-1, disp=True):
         # nr(l, graph_num)
         graph_num += 1
 
-    # The imploding stars for 3 graphs are 0, 6, 13 and 15
-
 
 # for 4 they are 0, 76, 176, 213, and 217
 def impStar(n):
@@ -284,119 +284,17 @@ def impStar(n):
 
 
 baseball = [15, 20, 39]
-pseduocycles = [27, ]
+pseduocycles = [27, 0]
 lines = [21]
 three_IS = [0, 6, 13, 15]
 four_IS = [0, 76, 176, 213, 217]
-# two exploding star for five vertices.
-# look at k exploding stars and lines
-# what are lines? We have a theory that they are either exploding stars
-# or they are k -- imploding stars unioned with disjoint isolated vertices.
 
-# check to see if the 2 exploding star on n vertices is always the line from 0 to n
-# or it's the 3 exploding star, or it's the n -2 exploding
-
-######RESULTS######
-# it is the 2 exploding that has the line from [0,n]
-# what about imploding stars unioned with isolated vertices?
-# for n= 3, imploding(1)  + isolated is just one edge nr = elipse
-# imploding(2) + isolated is 2 cycle, [0, 2]
-# imploding(3) + doesn't exist
-# for n = 4, imploding(1) + isolated is qnr([[1,-1,0,0],[0,1,-1,0],[0,0,0,0],[0,0,0,0]]), lopsided elipse, imploding(2) + isolated is qnr([[2,-1,-1,0],[0,1,-1,0],[0,-1,1,0],[0,0,0,0]]), ellipse, imploding(3) + isolated is
-
-
-# to finish my proof to show that the eigenvalues are real, take the matrix in frobenius
-# normal form, and then look at it's imploding k complete graph sections and then its
-# pointing to everything else section. One you know the form of, the other is just a upper triangularmatrix so
-# you know the eignvalues are k because the diangonal lines are k
-
-# isoQNR(4, r_graph_num= 76)
-# isoQNR(4, r_graph_num = four_IS)
-# isoQNR(3, r_graph_num = [0, 2, 3, 6, 8, 10, 13, 15])
-# isoQNR(3)
-# qnr(np.asarray([[5,-1,-1,-1,-1,-1],[-1,5,-1,-1,-1,-1],[-1,-1,5,-1,-1,-1],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]), -1, True)
-# isoQNR(4, 125)
-# qnr(np.asarray([[1,-1,0],[-1,2,-1],[0,-1,1]]), -1, True)
-# qnr(np.asarray([[1,-1,0,0,0],[-1,2,-1,0,0],[0,-1,2,-1,0],[0,0,-1,2,-1],[0,0,0,-1,1]]),-1,True)
-# cycle polygon
-
-
-# qnr(np.asarray([[1, 0, 0, 0, -1], [-1, 1, 0, 0, 0], [0, -1, 1, 0, 0], [0, 0, -1, 1, 0], [0, 0, 0, -1, 1]]), -1, True)
-# qnr(np.asarray([[1, 0, 0, -1], [0, 2, -1, -1], [-1, 0, 2, -1], [-1, 0, 0, 1]]), -1, True)
-
-# isoQNR(4)
-allQNR(4, disp=True)
-# isoQNR(4, [0, 3, 4, 19, 21, 23, 56, 62, 76, 79, 86, 91, 94, 117, 120, 125, 142, 176, 196, 203, 205, 213, 217])
-# isoQNR(4, [0, 3, 4, 19, 21, 23, 56, 62, 76, 79, 86, 91, 94, 117, 120, 125, 142, 176, 196, 203, 205, 213, 217])
+all_qnr(4, r_graph_num=82, disp=True)
 print('the singletons are at', singleton_index_list)
 print('the lines are at', line_index_list)
 print('the polygons are at', poly_index_list)
-# impStar(4)
 
-##TODO
-# could save gl's, so we can view all quicker
-# would seem to be a contradiction between the fact that the 2 imploding has a singleton, the isolated has nothing, but their union is an ellipse. Maybe the arguement of unions doesn't apply so well because we're taking NR(Q^TLQ), not just NR(L)
-####TESTING ON N=3 for lines#######
-# where is the line from 0 to n? -> 2 exploding, undescribed
-# for n = 3
-# lines are at [0, 2, 3, 6, 8, 10, 13, 15]
-# empty (gn 0), connected exploding (gn 13), fc (gn 15) is point
-# 1 exploding(gn 2), 2 cycle (gn 3), is [0,2]
-# complex line [-2.5i to 2.5i] (gn 6)
-# 2 exploding (gn 8), undescribed (FC minus di-edge) (gn 10) is [1 to n(3)]
-
-# We expected the [0,n] lines to come from (gn3), which is the two exploding star
-# plus an isolated vertex, but this is only 0 to 2. It is also a cycle.
-#
-
-# g = [[ 0  0  0  0],
-# [ 0  0  0  0],
-# [-1 -1  3 -1],
-# [-1 -1 -1  3]]
-########TESTING ON N=4 for lines#######
-# for n = 4
-# lines are at [0, 3, 4, 19, 21, 23, 56, 62, 76, 79, 86, 91, 94, 117, 120, 125, 142, 176, 196, 203, 205, 213, 217]
-# the lines we are looking for are at
-# gn 21, which is 2 exploding
-# gn 86, which is undescribed? 0 imploded upon, 3 exploding, 1 and 2 exploding
-# gn 91, FC(3) + exploding(1).
-# gn 94, flux capacitor full flow: exploding(1) spider?
-# gn 142, FC(3) + 2cycle
-# gn 196, imploding(2) + 2cycle on imploders
-# gn 203, diedge four cycle
-# gn 205, fc - diedge
-# list of lines from [0,n] = 21
-# list of lines from [1, n] = [3, 86, 91, 94, 142]
-# [2,n] = 196, 203, 205
-# [0,n-1] = [23, 56, 62, 79, 117]
-# [0,n-2] = [4,19, 120]
-# [.5, 3.5] = [125]
-
-[4, 21, 86, 91, 94, 142, 196, 203, 205]
-
-##parameterize the x and y in the complex plane by t,
-# solve for t from x, then check to see if that t gives the
-# correct y
-
-# line from point a = (a_1,a_2) to b = (b_1,b_2) is c = (a_1,a_2) - t(b_1 - a1, b2 - a2)
-
-# save the eigenvectors associated with polygons for alex
-# L1 = np.asarray([
-#     [1, 0, 0, 0, -1],
-#     [0, 1, 0, 0, -1],
-#     [0, 0, 1, 0, -1],
-#     [0, 0, 0, 1, -1],
-#     [-1, -1, -1, -1, 4]])
-# L2 = np.asarray([[2, - 1, 0, -1, 0],
-#                  [-1, 2, -1, 0, 0],
-#                  [0, -1, 2, -1, 0],
-#                  [-1, 0, -1, 2, 0],
-#                  [0, 0, 0, 0, 0]])
-# print('graph 1')
-# nr(L1, 1, True)
-# print('graph 2')
-# nr(L2, 2, True)
-
+#  [4, 21, 86, 91, 94, 142, 196, 203, 205]
 
 # problem with graph 208 on four vertices, the point is (2,0) but roundoff causes it to have a matrix of
 # rank 2
